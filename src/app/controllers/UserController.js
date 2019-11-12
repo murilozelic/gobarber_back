9c9af2c50d15988a329b15762798c4b4b731894c
Murilo Zelic <murilozelic@gmail.com>
2019-11-11 21:41:33 -0300

    Seguindo app

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -78,6 +78,11 @@ class UserController {
       provider,
     });
   }
+
+  async index(req, res) {
+    const users = await User.findAll({ attributes: ['id', 'name', 'email'] });
+    return res.json(users);
+  }
 }
 
 export default new UserController();
